868f2b26f0c5e9a1e4d371163c2485845e8f188a
danisylvester <maria.sylvester10@gmail.com>
2020-08-25 11:52:16 -0500

    asked user for fav subject & created a switch statement to print a custom statement.

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace SelectionStatements
             var r = new Random();
             var favNum = r.Next(1, 1000);
             bool rightAnswer = false;
-
+            /*
             do
             {
                 Console.WriteLine("Guess my favorite number.");
@@ -30,7 +30,34 @@ namespace SelectionStatements
                     rightAnswer = true;
                 }
             } while (rightAnswer == false);
+            */
 
+            // Printing statement based on favorite school subject
+            Console.WriteLine("Enter the number beside your favorite school subject!");
+            Console.WriteLine("1 Math \n" + "2 English \n" + "3 History \n" + "4 Science \n" + "5 PE \n");
+            int favSubj = int.Parse(Console.ReadLine());
+
+            switch(favSubj)
+            {
+                case 1:
+                    Console.WriteLine("You math nerd!");
+                    break;
+                case 2:
+                    Console.WriteLine("So you like English?");
+                    break;
+                case 3:
+                    Console.WriteLine("You history buff!");
+                    break;
+                case 4:
+                    Console.WriteLine("You science wizz!");
+                    break;
+                case 5:
+                    Console.WriteLine("So you workout?");
+                    break;
+                default:
+                    Console.WriteLine("Not a fan of school, huh?");
+                    break;
+            }
 
         }
     }
