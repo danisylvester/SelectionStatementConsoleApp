41ee7f63ab5b25f2f36fcfdf5ff40d08dfe8269d
danisylvester <maria.sylvester10@gmail.com>
2020-08-25 10:49:45 -0500

    added if else statement. User guesses random num between 1-1000

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNum = r.Next(1, 1000);
+
+            Console.WriteLine("Guess my favorite number.");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if(userInput == favNum)
+            {
+                Console.WriteLine("You got it!");
+                Console.WriteLine($"My favorite number is {favNum}.");
+            }
+            else if(userInput < favNum)
+            {
+                Console.WriteLine("Your guess is too low.");
+                Console.WriteLine($"My favorite number is {favNum}.");
+            }
+            else
+            {
+                Console.WriteLine("Your guess is too high.");
+                Console.WriteLine($"My favorite number is {favNum}.");
+            }
+        }
+    }
+}
